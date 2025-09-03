0f5ad3ad0629a0bbaaf86b66592951e970cdd07b
MuhammadTalha013 <m.talhaabdullah01@gmail.com>
2025-09-03 17:04:22 +0500

    Little changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,11 @@ return (
 <div>
 <h1 className="text-2xl font-bold mb-4">About</h1>
 <p className="text-gray-600">
-Travel Planner helps you explore destinations, view details, and save trips for later.
+Travel Plan is a user-friendly city exploration app that helps you discover and organize your travel interests with ease. Simply type the name of any city, and the app instantly displays its current weather, a popular landmark image, and a map view for quick reference.
+
+One of the key features of Travel Plan is its ability to save your favorite locations directly in your browser using local storage—no sign-up or account needed. This means you can quickly create your own list of must-visit cities and access them anytime you return.
+
+Whether you're planning your next trip, exploring potential destinations, or just curious about the weather in another part of the world, Travel Plan makes it simple, visual, and efficient..
 </p>
 </div>
 );
